133770cb2d7245727b4b6994e3e94caf13df2d01
Mominur Rahman <mominurrohman774@gmail.com>
2023-06-22 00:08:08 +0600

    fix empty issues

--- chat/templatetags/last_message.py
+++ chat/templatetags/last_message.py
@@ -6,5 +6,8 @@ register = template.Library()
 @register.filter(name='last_message')
 def get_last_message(user, recipient):
     room = Room.objects.filter(users=user).filter(users=recipient)
-    msg = Message.objects.filter(room=room.first()).last().message
-    return msg
+    msg = Message.objects.filter(room=room.first())
+    if msg.exists():
+        return msg.last().message
+    else:
+        return "Last message goes here"
